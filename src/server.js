c5de9d4f665d5002cddbe821a9af6977d4cec6c4
zhengp <zhengp>
2020-09-17 13:32:07 +0800

    [add] 新增日志功能

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser'); /*post方法*/
 const swagger = require('express-swagger-generator')
+const morgan = require('morgan')
+const FileStreamRotator = require('file-stream-rotator')
+const fs = require('fs')
+const path = require('path')
 
 const initControllers = require('./controller/index.js');
 
@@ -10,6 +14,26 @@ server.use(bodyParser.json()); // 添加json解析
 server.use(bodyParser.urlencoded({
     extended: false
 }));
+process.env.TZ = 'Asia/Shanghai';
+// 日志模块配置
+const logDirectory = path.join(__dirname,'log')
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+
+const accessLogStream = FileStreamRotator.getStream({
+    date_format: 'YYYYMMDD',
+    filename: path.join(logDirectory, 'access-%DATE%.log'),
+    // frequency: 'daily',
+    verbose: true
+})
+/*  自定义token
+morgan.token('from', function(req, res){
+    return req.query.from || '-';
+});
+ 自定义format，其中包含自定义的token
+morgan.format('joke', '[joke] :method :url :status :from'); 
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log')) */
+server.use(morgan('combined',{stream: accessLogStream}))
+
 const port = parseInt(process.env.PORT || 9000)
 
 // 加载主外键关系及创建数据库
