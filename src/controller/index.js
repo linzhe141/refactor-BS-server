d8ffd2d0ae07700968f536ef6d3a3070a47c331e
zhengp <zhengp>
2020-09-18 11:05:56 +0800

    [add] 新增班级管理模块的业务层和模型

--- src/controller/index.js
+++ src/controller/index.js
@@ -2,11 +2,13 @@ const {Router} = require('express')
 const userController = require('./user')
 const studentController = require('./student')
 const teacherController = require('./teacher')
+const classgradeController = require('./classgrade')
 
 module.exports = async function initControllers(){
     const router =  Router()
     router.use('/api/user', await userController())
     router.use('/api/student', await studentController())
     router.use('/api/teacher', await teacherController())
+    router.use('/api/classgrade', await classgradeController())
     return router
 }
---
+++ src/service/classgrade.js
@@ -0,0 +1,89 @@
+var Classgrade = require('../model/classgrade')
+var sequelize = require('../../config/sequelize.config')
+var Sequelize = require('sequelize')
+var Op = Sequelize.Op
+class ClassgradeService{
+    async findAll(){
+        let result
+        try {
+            result = await Classgrade.findAll()
+        } catch(error){
+            console.log('error-->',error)
+            return error
+        }
+        return result
+    }
+
+    async create({classNum,className}){
+        if(classNum && className){
+            let result
+            try { 
+                result = await Classgrade.create({classNum,className})
+            } catch(error){
+                return error
+            }
+            return result
+        }
+    }
+
+    async find({id,classNum,className}){
+        let result
+        if(id || classNum || className){
+            id = id || ''
+            classNum = classNum || ''
+            className = className || ''
+            try {
+                const params = {
+                    id: {[Op.like]: `%${id}%`},
+                    classNum: {[Op.like]: `%${classNum}%`},
+                    className: {[Op.like]: `%${className}%`},
+                }
+                result = await Classgrade.findAll({
+                    where: params
+                })
+            } catch (error) {
+                return error
+            }
+            return result
+        }
+    }
+
+    async update({id,classNum,className}){
+        let result
+        try {
+            result = await Classgrade.update({
+                classNum,className
+            },
+            {
+                where: {id}
+            })
+        } catch (error) {   
+            return error
+        }
+        return result
+    }
+
+    async deleteById({id}){
+        let result 
+        try {
+            result = await Classgrade.destroy({
+                where: {
+                    id
+                }
+            })
+        } catch (error) {
+            console.log('error--',error)
+            return error
+        }
+        return result
+    }
+}
+
+// 单例模式
+let service;
+module.exports = async function () {
+  if (!service) {
+    service = new ClassgradeService();
+  }
+  return service;
+};
